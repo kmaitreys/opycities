9e42bb6a65c505498c98f013b5f6b0bcd06714fe
kmaitreys <peakcipher1729@gmail.com>
2023-11-01 03:31:42 +0530

    created python project

---
+++ opycities/opycities.py
@@ -0,0 +1,11 @@
+class Model:
+    def __init__(self):
+        self.amin = 0.05
+        self.amax = 3000.0
+        self.apow = 3.5
+        self.amean = 0.0
+        self.asig = 0.0
+        self.na = 0
+
+
+model = Model()
