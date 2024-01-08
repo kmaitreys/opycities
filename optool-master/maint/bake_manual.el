598ecc9622f55d629ba4a096daa087a4b9f26705
kmaitreys <peakcipher1729@gmail.com>
2024-01-08 20:49:18 +0530

    moved to mirror

--- optool-master/maint/bake_manual.el
+++
@@ -1,8 +0,0 @@
-  (eval-after-load "ox-latex"
-    '(add-to-list 'org-latex-classes
-                  '("koma-article" "\\documentclass{scrartcl}"
-                    ("\\section{%s}" . "\\section*{%s}")
-                    ("\\subsection{%s}" . "\\subsection*{%s}")
-                    ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
-                    ("\\paragraph{%s}" . "\\paragraph*{%s}")
-                    ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
