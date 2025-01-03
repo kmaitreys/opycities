1de427bdf05ed8ed95047ad1c24cbe1c39f38a67
kmaitreys <km.maitrey@gmail.com>
2025-01-03 11:41:36 +0530

    refac: new start

--- opycities/constants.py
+++
@@ -1,4 +0,0 @@
-import astropy.constants as const
-
-SPEED_OF_LIGHT_CGS = const.c.cgs.value
-SPEED_OF_LIGHT_SI = const.c.value
---
+++ src/Opacities.jl
@@ -0,0 +1,5 @@
+module Opacities
+
+greet() = print("Hello World!")
+
+end # module Opacities
